9fd81f85794e9e06ca35015d70c57119b2d8457c
Pawel Tschudnowsky <pawel.tschudnowsky@tomcom.de>
2019-03-20 16:01:14 +0100

    reproduce androidx and javaCompileOptions bugs

---
+++ app/src/main/java/com/photoeditorsdk/android/app/TestBean.java
@@ -0,0 +1,14 @@
+package com.photoeditorsdk.android.app;
+
+import org.androidannotations.annotations.EBean;
+
+
+/**
+ * User: pavel
+ * Date: 20.03.19
+ * Time: 14:06
+ */
+@EBean
+public class TestBean
+{
+}
